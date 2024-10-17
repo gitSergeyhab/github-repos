cd55b1eaec0ba08ceb4044ce699d0fbb990d6c52
Sergey <redmoloch@yandex.ru>
2024-10-17 12:12:51 +0300

    feat(gh-list): Доработать nginx

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ docker build -t gh-list-image .
 ## Пример запуска контейнера (на 80 порту)
 
 ```bash
-docker run -d --name gh-list-cont -p 80:80 gh-list-image
+docker run -d --name gh-list-cont -p 80:80 -e GITHUB_USER=gitSergeyhab gh-list-image
 ```
--- src/hooks/use-fetch-repos.ts
+++ src/hooks/use-fetch-repos.ts
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react";
 import { Repo, Status } from "../types";
 
-const BASE_URL =
-  import.meta.env.VITE_BASE_URL ||
-  "https://api.github.com/users/gitSergeyhab/repos";
-
-export const useFetchRepos = (count: number) => {
+export const useFetchRepos = (count?: number) => {
   const [repos, setRepos] = useState<Repo[]>([]);
   const [status, setStatus] = useState<Status>("idle");
 
   useEffect(() => {
     setStatus("loading");
-    fetch(BASE_URL + `?per_page=${`${count}&page=1&sort=updated`}`)
+    fetch(`api?per_page=${count}&page=1&sort=updated`)
       .then((res) => {
         if (res.ok) {
           return res.json();
